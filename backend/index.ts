3174fafc079c234226ca4705e5b1e5adcba371c8
Kévin DANG <kdang@pitchy.fr>
2022-09-03 01:01:47 +0200

    setup backend

---
+++ backend/index.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { container } from "./src/IoC/container";
+import { InversifyExpressServer } from "inversify-express-utils";
+import cors from "cors"
+import bodyParser from "body-parser";
+
+const port = process.env.PORT || 8000 ;
+
+// create server
+let server = new InversifyExpressServer(container);
+server.setConfig((app) => {
+    app.use(cors())
+    // add body parser
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+});
+
+let app = server.build();
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+})
