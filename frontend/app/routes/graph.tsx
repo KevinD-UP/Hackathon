60c8ae91288ea9baff3d4780fa5b38d42123a941
Robin Arnoux <robinjarnoux@gmail.com>
2022-09-06 15:56:31 +0200

    change scales graph + working on graph time scale

--- frontend/app/routes/graph.tsx
+++ frontend/app/routes/graph.tsx
@@ -4,16 +4,28 @@ import {
     YAxis,
     HorizontalGridLines,
     VerticalGridLines,
-    LineSeries
+    LineSeries,
 } from 'react-vis';
-import React, { useState, useCallback, CSSProperties, useEffect } from 'react'
-import { useTransition, animated, AnimatedProps, useSpringRef } from '@react-spring/web'
+import React, { useState, useCallback, CSSProperties, useEffect } from 'react';
+import { useTransition, animated, AnimatedProps, useSpringRef } from '@react-spring/web';
+
+import {timeFormatDefaultLocale} from 'd3-time-format';
+timeFormatDefaultLocale({
+    dateTime    : '%a %b %e %X %Y',
+    date        : '%d/%m/%Y',
+    time        : '%H : %M : %S',
+    periods     : ['AM', 'PM'],
+    days        : ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+    shortDays   : ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'],
+    months      : ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Decembre'],
+    shortMonths : ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jui', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec']
+});
 
 const pages: ((props: AnimatedProps<{ style: CSSProperties }>) => React.ReactElement)[] = [
     ({ style }) => <animated.div style={{ ...style, background: 'lightpink' }}>A</animated.div>,
     ({ style }) => <animated.div style={{ ...style, background: 'lightblue' }}>B</animated.div>,
     ({ style }) => <animated.div style={{ ...style, background: 'lightgreen' }}>C</animated.div>,
-]
+];
 
 const data = [
     {
@@ -33,19 +45,20 @@ const data = [
 const tickValues = [-20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 
 export default function Graph() {
-    const [index, set] = useState(0)
-    const onClick = useCallback(() => set(state => (state + 1) % 3), [])
-    const transRef = useSpringRef()
+    const [index, set] = useState(0);
+    const onClick = useCallback(() => set(state => (state + 1) % 3), []);
+    const transRef = useSpringRef();
     const transitions = useTransition(index, {
         ref: transRef,
         keys: null,
         from: { opacity: 0, transform: 'translate3d(100%,0,0)', width: 200},
         enter: { opacity: 1, transform: 'translate3d(0%,0,0)' , width: 200},
         leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' , width: 200},
-    })
+    });
     useEffect(() => {
         transRef.start()
-    }, [index])
+    }, [index]);
+
     return (
         <div className='flex'>
             <div className={`flex fill articles`} onClick={onClick}>
@@ -55,15 +68,15 @@ export default function Graph() {
                 })}
             </div>
             <div>
-                <XYPlot height={300} width={370} margin={{left: 70}} xType='ordinal'>
+                <XYPlot height={300} width={370} margin={{left: 70}} xType={'ordinal'} yType={'time'}>
                     <VerticalGridLines/>
                     <HorizontalGridLines/>
                     <XAxis title="Temps"/>
-                    <YAxis title="Température °C"  tickValues={tickValues} left={-30}/>
-                    <LineSeries data={[{ x: 0, y: 0 }]} style={{ display: 'none' }} />
+                    <YAxis title="Température °C"  left={-30}/>
+                   <LineSeries data={[{ x:0, y: 0 }]} style={{ display: 'none' }} />
                     <LineSeries data={data} curve={'curveMonotoneX'} style={{fill: 'none', strokeWidth: 4}}/>
                 </XYPlot>
             </div>
         </div>
-    )
+    );
 }
