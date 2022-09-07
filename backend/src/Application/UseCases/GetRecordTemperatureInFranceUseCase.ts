7d69887468594e2218288fd379326855d84aa112
Pholasa <pholasa.remy@outlook.fr>
2022-09-07 11:51:12 +0200

    Temperature avec la date

--- backend/src/Application/UseCases/GetRecordTemperatureInFranceUseCase.ts
+++ backend/src/Application/UseCases/GetRecordTemperatureInFranceUseCase.ts
@@ -9,8 +9,14 @@ export class GetRecordTemperatureInFranceUseCase {
     8.4, 10.1, 11, 14.7, 17.8, 22.4, 24.4, 24.8, 20.3, 16.3, 11.4, 9.5,
   ];
 
-  async execute(month: Text): Promise<Temperature> {
-    const monthInt: number = +month.text;
-    return { temperatureRecord: this.record[monthInt] };
+  async execute(date: Text): Promise<Temperature> {
+    if (date.text.length > 3) {
+      const divide = date.text.split("-");
+      const monthInt: number = +divide[1] - 1;
+      return { temperatureRecord: this.record[monthInt] };
+    } else {
+      const monthInt: number = +date.text - 1;
+      return { temperatureRecord: this.record[monthInt] };
+    }
   }
 }
